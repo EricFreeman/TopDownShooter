d2a67f44f805072df06020672a3ec9308b30ec22
Eric Freeman <eric.freeman@extendhealth.com>
2015-02-12 23:43:31 -0700

    Added sprite for player and made him walk with simple script.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Player : MonoBehaviour
+    {
+        public float Speed;
+
+        void Update()
+        {
+            var frameSpeed = Speed*Time.deltaTime;
+
+            transform.Translate(Input.GetAxisRaw("Horizontal") * frameSpeed, Input.GetAxisRaw("Vertical") * frameSpeed, 0);      
+        }
+    }
+}
