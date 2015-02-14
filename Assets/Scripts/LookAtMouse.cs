34051fa1cfe62a7a2a4bfe165b564634fca83655
Eric Freeman <eric.freeman@extendhealth.com>
2015-02-13 22:54:59 -0700

    Added scripts for looking at mouse and looking at specific point.

---
+++ Assets/Scripts/LookAtMouse.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class LookAtMouse : MonoBehaviour
+    {
+        void FixedUpdate()
+        {
+            var playerPlane = new Plane(Vector3.forward, transform.position);
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float hitdist;
+            if (playerPlane.Raycast(ray, out hitdist))
+            {
+                var targetPoint = ray.GetPoint(hitdist);
+                
+                var newRotation = Quaternion.LookRotation(transform.position - targetPoint, Vector3.forward);
+                newRotation.x = 0;
+                newRotation.y = 0;
+                transform.rotation = newRotation;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/LookAtPoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class LookAtPoint : MonoBehaviour
+    {
+        public float RotationSpeed = 8;
+        public Vector3 TargetPoint;
+
+        void FixedUpdate()
+        {
+            var newRotation = Quaternion.LookRotation(transform.position - TargetPoint, Vector3.forward);
+            newRotation.x = 0;
+            newRotation.y = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * RotationSpeed);
+        }
+    }
+}
