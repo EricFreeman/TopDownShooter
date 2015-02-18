1c8d98e1355bc73a9df04f4c4f0c878f1e860830
Eric Freeman <eric.freeman@extendhealth.com>
2015-02-17 23:02:48 -0700

    Okay, made director that spawns enemies every 5 seconds.

---
+++ Assets/Scripts/Director.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Director : MonoBehaviour
+    {
+        public GameObject Enemy;
+        public float SpawnDelay = 5;
+        public float CurrentSpawnDelay;
+
+        void Start()
+        {
+            CurrentSpawnDelay = SpawnDelay;
+        }
+
+        void Update()
+        {
+            CurrentSpawnDelay -= Time.deltaTime;
+            if (CurrentSpawnDelay <= 0)
+            {
+                CurrentSpawnDelay += SpawnDelay;
+                var newEnemy = (GameObject) Instantiate(Enemy);
+            }
+        }
+    }
+}
