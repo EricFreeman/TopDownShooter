0c5c0b067b6c2bdc0504dfd6f0894089259239d7
Eric Freeman <eric.freeman@extendhealth.com>
2015-03-25 18:37:02 -0600

    Added script to remove bullets when they go offscreen.

---
+++ Assets/Scripts/RemoveOffscreen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class RemoveOffscreen : MonoBehaviour
+    {
+        void Update()
+        {
+            // 1 sec check is a hack to fix it removing all npcs on load
+            if (!GetComponent<Renderer>().isVisible && Time.fixedTime > 1)
+                DestroyImmediate(gameObject);
+        }
+    }
+}
