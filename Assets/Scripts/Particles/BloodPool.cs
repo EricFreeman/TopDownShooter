c710a4a4f88330b63db895fd72eaf3dd86a5dc01
Eric Freeman <eric.freeman@extendhealth.com>
2015-04-27 23:17:12 -0600

    Animated the blood a tad bit so it looks like it pools up when someone is hit.

---
+++ Assets/Scripts/Particles/BloodPool.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Particles
+{
+    public class BloodPool : MonoBehaviour
+    {
+        public float Speed = 3f;
+        private Vector3 _spread;
+
+        public void Setup(Vector3 spread)
+        {
+            _spread = spread;
+        }
+
+        void Update ()
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _spread, Speed * Time.deltaTime);
+
+            if(transform.position == _spread)
+                Destroy(this);
+        }
+    }
+}
--- Assets/Scripts/Particles/BloodSplat.cs
+++ Assets/Scripts/Particles/BloodSplat.cs
@@ -15,13 +15,15 @@ namespace Assets.Scripts.Particles
 
             for (var i = 0; i < amount; i++)
             {
-                var spreadX = Random.Range(-1f * extraBloodModifier, 1f * extraBloodModifier);
-                var spreadY = Random.Range(-1f * extraBloodModifier, 1f * extraBloodModifier);
+                var spreadX = Random.Range(-.5f * extraBloodModifier, .5f * extraBloodModifier);
+                var spreadY = Random.Range(-.5f * extraBloodModifier, .5f * extraBloodModifier);
+                var spead = new Vector3(spreadX, spreadY) + transform.position;
                 var size = Random.Range(.5f * extraBloodModifier, 2f);
                 var opacity = Random.Range(.25f, 1f);
 
                 var b = Instantiate(BloodGameObject);
-                b.transform.position = transform.position + new Vector3(spreadX, spreadY, 0);
+                b.GetComponent<BloodPool>().Setup(spead);
+                b.transform.position = transform.position;
                 b.transform.localScale = new Vector3(size, size, size);
                 b.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, opacity);
                 b.GetComponent<SpriteRenderer>().sortingOrder = Constants.BloodLayer;
