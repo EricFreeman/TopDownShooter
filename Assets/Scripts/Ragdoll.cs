c68f7a4f1749758ffb90ac54b982184de396a450
Kevin L. Owens <kowens@gmail.com>
2015-06-22 22:28:16 -0600

    I am experimenting with a city level.

---
+++ Assets/Scripts/Ragdoll.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ragdoll : MonoBehaviour
+{
+    public Component[] bones;
+    public Animator anim;
+    public bool isDead;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        bones = gameObject.GetComponentsInChildren<Rigidbody>();
+        anim = GetComponent<Animator>();
+
+
+    }
+    void Update()
+    {
+        if (isDead)
+            killRagdoll();
+
+    }
+
+    // Update is called once per frame
+    void killRagdoll()
+    {
+        foreach (Rigidbody ragdoll in bones)
+        {
+            ragdoll.isKinematic = false;
+        }
+
+        anim.enabled = false;
+    }
+}
