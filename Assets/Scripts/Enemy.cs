54368230698e5de6553b0c2c99c5ba571c24c1c5
Kevin L. Owens <kowens@gmail.com>
2015-07-01 20:25:11 -0600

    When a zombie touches the player, have the player appear to get hurt.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,23 +11,51 @@ namespace Assets.Scripts
 
         void OnCollisionEnter(Collision collision)
         {
-            var bullet = collision.gameObject.GetComponent<Bullet>();
-            if (bullet != null)
+            if (collision.collider.name == "Sprite")
             {
-                Health -= bullet.Damage;
-
-                EventAggregator.SendMessage(new SpawnDamageTextMessage { Position = transform.position, Text = bullet.Damage.ToString("N0") });
-
-                if (Health > 0)
+                var playerPosition = collision.collider.transform.position;
+                EventAggregator.SendMessage(new SpawnDamageTextMessage
                 {
-                    EventAggregator.SendMessage(new SpawnGibsMessage { Count = 1, Position = transform.position });
-                    EventAggregator.SendMessage(new SpawnBloodMessage { Position = transform.position });
-                }
-                else
+                    Position = playerPosition,
+                    Text = "Ow!"
+                });
+                EventAggregator.SendMessage(new SpawnDamageTextMessage
                 {
-                    Destroy(gameObject);
-                    EventAggregator.SendMessage(new SpawnBloodMessage { Position = transform.position, SplatterSize = SplatterSize.Medium });
-                    EventAggregator.SendMessage(new SpawnGibsMessage { Count = 5, Position = transform.position });
+                    Position = transform.position,
+                    Text = "Ow!"
+                });
+                EventAggregator.SendMessage(new SpawnGibsMessage { Count = 1, Position = playerPosition });
+                EventAggregator.SendMessage(new SpawnBloodMessage { Position = playerPosition });
+                
+            }
+            else
+            {
+                var bullet = collision.gameObject.GetComponent<Bullet>();
+                if (bullet != null)
+                {
+                    Health -= bullet.Damage;
+
+                    EventAggregator.SendMessage(new SpawnDamageTextMessage
+                        {
+                            Position = transform.position,
+                            Text = bullet.Damage.ToString("N0")
+                        });
+
+                    if (Health > 0)
+                    {
+                        EventAggregator.SendMessage(new SpawnGibsMessage {Count = 1, Position = transform.position});
+                        EventAggregator.SendMessage(new SpawnBloodMessage {Position = transform.position});
+                    }
+                    else
+                    {
+                        Destroy(gameObject);
+                        EventAggregator.SendMessage(new SpawnBloodMessage
+                            {
+                                Position = transform.position,
+                                SplatterSize = SplatterSize.Medium
+                            });
+                        EventAggregator.SendMessage(new SpawnGibsMessage {Count = 5, Position = transform.position});
+                    }
                 }
             }
         }
